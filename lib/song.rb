58c240dd1f07d8778ff83eec2b65e8f150b8d802
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-13 22:46:06 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song 
+ attr_accessor :name, :artist, :genre 
+ 
+ @@count = 0 
+ @@artists = []
+ @@genres = []
+ 
+  def initialize (name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+    @@artists.push artist 
+    @@genres.push genre 
+  end
+  def self.count
+    @@count 
+  end 
+  def self.artists
+    @@artists.uniq 
+  end 
+  def self.genres 
+    @@genres.uniq 
+  end 
+  def self.genre_count
+    new_hash = {}
+    @@genres.each do |g|
+      if new_hash[g]
+        
+      else
+      end 
+    end 
+  end 
+end 
+
+ 
